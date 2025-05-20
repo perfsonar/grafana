b6b5f5ff625ec36f4180447c52b2de25fff48192
Andy Lake <andy@es.net>
2025-05-20 09:43:41 -0400

    Bumping version of Opensearch and Matrix plugins

--- perfsonar-grafana/perfsonar-grafana/grafana_common.sh
+++ perfsonar-grafana/perfsonar-grafana/grafana_common.sh
@@ -6,8 +6,8 @@ ARCH=$(uname -m | sed -e 's/x86_64/amd64/')
 #
 # PLUGINS_TO_INSTALL: Associative array that maps local name to download URL
 declare -A PLUGINS_TO_INSTALL
-PLUGINS_TO_INSTALL[grafana-opensearch-datasource]="https://grafana.com/api/plugins/grafana-opensearch-datasource/versions/2.13.1/download?os=linux&arch=${ARCH}"
-PLUGINS_TO_INSTALL[grafana-esnet-matrix-panel-viz]="https://grafana.com/api/plugins/esnet-matrix-panel/versions/1.0.9/download"
+PLUGINS_TO_INSTALL[grafana-opensearch-datasource]="https://grafana.com/api/plugins/grafana-opensearch-datasource/versions/2.26.1/download?os=linux&arch=${ARCH}"
+PLUGINS_TO_INSTALL[grafana-esnet-matrix-panel-viz]="https://grafana.com/api/plugins/esnet-matrix-panel/versions/1.0.10/download"
 
 ##
 # PLUGINS_TO_REMOVE: Array with the names of the unzipped directories to be removed on install. 
